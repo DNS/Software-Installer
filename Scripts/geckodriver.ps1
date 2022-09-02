09f9122427afa8e75484dadaac11f14eee5aa1d1
Daniel Sirait <dsirait@outlook.com>
2022-09-02 07:38:33 +0700

    -

--- Scripts/geckodriver.ps1
+++ Scripts/geckodriver.ps1
@@ -1,4 +1,5 @@
 #cls
+
 $r = iwr https://github.com/mozilla/geckodriver/releases/latest
 $x = $r -match '\<a href\=\"(.+?\-win64\.zip)\"'
 $url = 'https://github.com' + $Matches[1]
--- Scripts/radeon-pro.ps1
+++ Scripts/radeon-pro.ps1
@@ -1,8 +1,6 @@
-﻿
-#cls
+﻿#cls
 
 $r = iwr https://www.amd.com/en/support/graphics/radeon-400-series/radeon-rx-400-series/radeon-rx-470
-
 $m = [regex]::Matches($r, '\"(.+?\.exe)')
 
 foreach ($i in $m) {
