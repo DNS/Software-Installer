bbcc3be02a80a53bdee4c130ac0d897ce3305967
Daniel Sirait <dsirait@outlook.com>
2022-09-02 07:47:44 +0700

    -

--- Scripts/7-zip-gui.ps1
+++ Scripts/7-zip-gui.ps1
@@ -1,11 +1,11 @@
-cls
+#cls
 
 $r = iwr https://www.7-zip.org/download.html
 $m = [regex]::Matches($r, '\<A href\=\"(.+?\.msi)\"')
 
 foreach ($i in $m) {
     #$i.Groups[1].Value
-    if (($i.Groups[1].Value -imatch '64\-bit') -and ($i.Groups[1].Value -inotmatch 'Portable')) {
+    if ($i.Groups[1].Value -imatch '64\-bit' -and $i.Groups[1].Value -inotmatch 'Portable') {
         $url = $i.Groups[1].Value
         break
     }
