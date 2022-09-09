6a0db2aebeebf1658efcd03734dc6aa7322db221
Daniel Sirait <dsirait@outlook.com>
2022-09-09 14:11:03 +0700

    -

--- bulk-dl.ps1
+++ bulk-dl.ps1
@@ -16,31 +16,31 @@ Set-Location .\Installer
 
 
 # 7-zip gui
-#(Invoke-WebRequest -Uri https://www.7-zip.org/download.html).Links.href | %{ if ($_ -imatch '\-x64\.msi') {'https://www.7-zip.org/'+$_; break;} }
+(Invoke-WebRequest -Uri https://www.7-zip.org/download.html).Links.href | %{ if ($_ -imatch '\-x64\.msi') {'https://www.7-zip.org/'+$_; break;} } | %{ wget.exe -cnd $_ }
 
 # 7-zip console
-#(Invoke-WebRequest -Uri https://www.7-zip.org/download.html).Links.href | %{ if ($_ -imatch '\-extra\.7z') {'https://www.7-zip.org/'+$_; break;} }
+(Invoke-WebRequest -Uri https://www.7-zip.org/download.html).Links.href | %{ if ($_ -imatch '\-extra\.7z') {'https://www.7-zip.org/'+$_; break;} } | %{ wget.exe -cnd $_ }
 
 # Firefox
-#'https://download.mozilla.org/?product=firefox-esr-next-latest-ssl&os=win64&lang=en-US' | %{ wget.exe -cnd $_ }
+'https://download.mozilla.org/?product=firefox-esr-next-latest-ssl&os=win64&lang=en-US' | %{ wget.exe -cnd $_ }
 
 # Gecko Driver
-#(Invoke-WebRequest -Uri https://github.com/mozilla/geckodriver/releases/latest).Links.href | %{ if ($_ -imatch '\-win64\.zip') {'https://github.com/'+$_; break;} }
+(Invoke-WebRequest -Uri https://github.com/mozilla/geckodriver/releases/latest).Links.href | %{ if ($_ -imatch '\-win64\.zip') {'https://github.com/'+$_; break;} } | %{ wget.exe -cnd $_ }
 
 # Git
-#(Invoke-WebRequest -Uri https://git-scm.com/download/win).Links.href | %{ if ($_ -imatch '\-64\-bit\.exe') {$_; break;} }
+(Invoke-WebRequest -Uri https://git-scm.com/download/win).Links.href | %{ if ($_ -imatch '\-64\-bit\.exe') {$_; break;} } | %{ wget.exe -cnd $_ }
 
 # Tortoise Git
-#(Invoke-WebRequest -Uri https://tortoisegit.org/download/).Links.href | %{ if ($_ -imatch '\-64bit\.msi') {'https:'+$_; break;} }
+(Invoke-WebRequest -Uri https://tortoisegit.org/download/).Links.href | %{ if ($_ -imatch '\-64bit\.msi') {'https:'+$_; break;} } | %{ wget.exe -cnd $_ }
 
-# MS JDK , # Invoke-WebRequest Bug on PowerShell 5 ?
-#(Invoke-WebRequest -Uri 'https://docs.microsoft.com/en-us/java/openjdk/download').Links.href | %{ if ($_ -imatch '\-x64\.msi') {$_; break;} }
+# MS JDK , # Invoke-WebRequest Bug on PowerShell 5 !!
+(Invoke-WebRequest -Uri 'https://docs.microsoft.com/en-us/java/openjdk/download').Links.href | %{ if ($_ -imatch '\-x64\.msi') {$_; break;} } | %{ wget.exe -cnd $_ }
 
-# Radeon Pro , # Invoke-WebRequest Bug on PowerShell 7 ?
-#(Invoke-WebRequest -Uri 'https://www.amd.com/en/support/professional-graphics/amd-radeon-pro/amd-radeon-pro-w6000-series/amd-radeon-pro-w6800').Links.href | %{ if ($_ -imatch '\.exe') {$_; break;} }
+# Radeon Pro , # Invoke-WebRequest Bug on PowerShell 7 !!
+#(Invoke-WebRequest -Uri 'https://www.amd.com/en/support/professional-graphics/amd-radeon-pro/amd-radeon-pro-w6000-series/amd-radeon-pro-w6800').Links.href | %{ if ($_ -imatch '\.exe') {$_; break;} } | %{ wget.exe -cnd $_ }
 
 # Scite
-#(Invoke-WebRequest -Uri https://www.scintilla.org/SciTEDownload.html).Links.href | %{ if ($_ -imatch 'wscite\d+?\.zip') {$_; break;} }
+(Invoke-WebRequest -Uri https://www.scintilla.org/SciTEDownload.html).Links.href | %{ if ($_ -imatch 'wscite\d+?\.zip') {$_; break;} } | %{ wget.exe -cnd $_ }
 
 # sqlite
-#(Invoke-WebRequest -Uri https://www.sqlite.org/download.html).Links.href | %{ if ($_ -imatch 'sqlite\-tools\-.+?\.zip') {'https://www.sqlite.org/'+$_; break;} }
+(Invoke-WebRequest -Uri https://www.sqlite.org/download.html).Links.href | %{ if ($_ -imatch 'sqlite\-tools\-.+?\.zip') {'https://www.sqlite.org/'+$_; break;} } | %{ wget.exe -cnd $_ }
