98af41dc3f87b9b84932e4d0ac90ed3be485cb4f
Daniel Sirait <dsirait@outlook.com>
2022-09-10 16:10:57 +0700

    -

--- bulk-dl.ps1
+++ bulk-dl.ps1
@@ -34,10 +34,10 @@ Set-Location .\Installer
 (Invoke-WebRequest -Uri https://tortoisegit.org/download/).Links.href | %{ if ($_ -imatch '\-64bit\.msi') {'https:'+$_; break;} } | %{ wget.exe -cnd $_ }
 
 # MS JDK , # Invoke-WebRequest Bug on PowerShell 5 !!
-(Invoke-WebRequest -Uri 'https://docs.microsoft.com/en-us/java/openjdk/download').Links.href | %{ if ($_ -imatch '\-x64\.msi') {$_; break;} } | %{ wget.exe -cnd $_ }
+(Invoke-WebRequest -Uri https://docs.microsoft.com/en-us/java/openjdk/download).Links.href | %{ if ($_ -imatch '\-x64\.msi') {$_; break;} } | %{ wget.exe -cnd $_ }
 
 # Radeon Pro , # Invoke-WebRequest Bug on PowerShell 7 !!
-(Invoke-WebRequest -Uri 'https://www.amd.com/en/support/professional-graphics/amd-radeon-pro/amd-radeon-pro-w6000-series/amd-radeon-pro-w6800').Links.href | %{ if ($_ -imatch '\.exe') {$_; break;} } | %{ wget.exe -cnd $_ }
+(Invoke-WebRequest -Uri https://www.amd.com/en/support/professional-graphics/amd-radeon-pro/amd-radeon-pro-w6000-series/amd-radeon-pro-w6800).Links.href | %{ if ($_ -imatch '\.exe') {$_; break;} } | %{ wget.exe -cnd $_ }
 
 # Scite
 (Invoke-WebRequest -Uri https://www.scintilla.org/SciTEDownload.html).Links.href | %{ if ($_ -imatch 'wscite\d+?\.zip') {$_; break;} } | %{ wget.exe -cnd $_ }
@@ -58,9 +58,37 @@ https://ftp.osuosl.org/pub/xiph/releases/flac/
 # WinSCP
 (Invoke-WebRequest -Uri https://winscp.net/eng/download.php).Links.href | %{ if ($_ -imatch '\-Setup\.exe') {'https://winscp.net'+$_; break;} } | %{ wget.exe -cnd $_ }
 
+# curl & openssl
+(Invoke-WebRequest -Uri https://curl.se/windows/).Links.href | %{ if ($_ -imatch '\-win64\-mingw\.zip') {'https://curl.se/windows/'+$_; break;} } | %{ wget.exe -cnd $_ }
+
+# putty
+https://www.chiark.greenend.org.uk/~sgtatham/putty/
+
+# ImageMagick
+(Invoke-WebRequest -Uri https://imagemagick.org/script/download.php).Links.href | %{ if ($_ -imatch '\-Q16\-x64\-static\.exe') {$_; break;} } | %{ wget.exe -cnd $_ }
+
+# Paint .NET
+(Invoke-WebRequest -Uri https://github.com/paintdotnet/release/releases/latest).Links.href | %{ if ($_ -imatch '\.winmsi\.x64\.zip') {'https://github.com'+$_; break;} } | %{ wget.exe -cnd $_ }
+#paint.net.4.0.Install.exe [/skipConfig | /auto | /createMsi] [PROPERTY=VALUE...]
+
+# Winaero
+'https://winaerotweaker.com/download/' | %{ wget.exe -cnd $_ }
+
+
+# JDownloader2
+#https://jdownloader.org/download/index
+#(Invoke-WebRequest -Uri https://jdownloader.org/download/index).Links.href | %{ if ($_ -imatch '\-x64\.msi') {$_; break;} } | %{ wget.exe -cnd $_ }
+
+
+
+
+
+
 
 (Invoke-WebRequest -Uri https://www.7-zip.org/download.html).Links.href | %{ if ($_ -imatch '\-x64\.msi') {$_; break;} } | %{ wget.exe -cnd $_ }
 (Invoke-WebRequest -Uri https://www.7-zip.org/download.html).Links.href | %{ if ($_ -imatch '\-x64\.msi') {$_; break;} } | %{ wget.exe -cnd $_ }
+(Invoke-WebRequest -Uri https://www.7-zip.org/download.html).Links.href | %{ if ($_ -imatch '\-x64\.msi') {$_; break;} } | %{ wget.exe -cnd $_ }
+
 (Invoke-WebRequest -Uri https://www.7-zip.org/download.html).Links.href | %{ if ($_ -imatch '\-x64\.msi') {$_; break;} } | %{ wget.exe -cnd $_ }
 (Invoke-WebRequest -Uri https://www.7-zip.org/download.html).Links.href | %{ if ($_ -imatch '\-x64\.msi') {$_; break;} } | %{ wget.exe -cnd $_ }
 (Invoke-WebRequest -Uri https://www.7-zip.org/download.html).Links.href | %{ if ($_ -imatch '\-x64\.msi') {$_; break;} } | %{ wget.exe -cnd $_ }
